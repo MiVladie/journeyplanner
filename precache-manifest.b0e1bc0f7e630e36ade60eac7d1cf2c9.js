a1ef31c8ff8b2d616f21131f6526be9064dccef2
MiVladie <mivladie@github.com>
2019-11-09 19:23:54 +0000

    Updates

---
+++ precache-manifest.b0e1bc0f7e630e36ade60eac7d1cf2c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bac19d93e3f216412088e79be209568",
+    "url": "/journeyplanner/index.html"
+  },
+  {
+    "revision": "83693d2e209458a00b75",
+    "url": "/journeyplanner/static/css/main.b75ae371.chunk.css"
+  },
+  {
+    "revision": "4ab506ae6871b52a52c3",
+    "url": "/journeyplanner/static/js/2.efee8c24.chunk.js"
+  },
+  {
+    "revision": "83693d2e209458a00b75",
+    "url": "/journeyplanner/static/js/main.1dbaed1d.chunk.js"
+  },
+  {
+    "revision": "f3177354e239fc6d728a",
+    "url": "/journeyplanner/static/js/runtime-main.ffc22c5c.js"
+  },
+  {
+    "revision": "f407fbc9ddfae5a1a75fb89481ba7674",
+    "url": "/journeyplanner/static/media/background.f407fbc9.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/journeyplanner/precache-manifest.b0e1bc0f7e630e36ade60eac7d1cf2c9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/journeyplanner/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
